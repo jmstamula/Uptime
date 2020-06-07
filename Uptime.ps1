d2b197abc021dd808ce98eca65dd5d5537edcea8
JM <54837852+jmstamula@users.noreply.github.com>
2020-06-07 20:33:32 +0800

    Update Uptime.ps1

--- Uptime.ps1
+++ Uptime.ps1
@@ -1,2 +1,12 @@
-(Get-Date) - [Management.ManagementDateTimeConverter]::ToDateTime((Get-WmiObject Win32_OperatingSystem -ComputerName VWNV02AX02185).LastBootUpTime) ` 
-| Select-Object -Property Days,Hours,Minutes | FL
+$SecurePassword = $env:Password | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:User, $SecurePassword
+$Computers = ($env:Computer).Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
+
+Foreach ($computer in $Computers)
+ {"Working on Computer $computer, please wait..."
+ 
+ "$computer"
+ (Get-Date) - [Management.ManagementDateTimeConverter]::ToDateTime((Get-WmiObject Win32_OperatingSystem -ComputerName $computer -Credential $cred).LastBootUpTime)  `
+    | Select-Object -Property Days,Hours,Minutes | FL
+
+}
