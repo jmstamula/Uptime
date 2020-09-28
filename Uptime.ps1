6807c2f682e0c03d421545cc578b4efcb9bc94a0
JM <54837852+jmstamula@users.noreply.github.com>
2020-09-28 11:42:41 +0800

    Update Uptime.ps1

--- Uptime.ps1
+++ Uptime.ps1
@@ -2,9 +2,11 @@ $SecurePassword = $env:Password | ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:User, $SecurePassword
 $Computers = ($env:Computer).Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
 
-Foreach ($computer in $Computers)
- {"System Uptime:$computer"
-  (Get-Date) - [Management.ManagementDateTimeConverter]::ToDateTime((Get-WmiObject Win32_OperatingSystem -ComputerName $computer -Credential $cred).LastBootUpTime)  `
-    | Select-Object -Property Days,Hours,Minutes | FL
+Foreach ($computer in $Computers){
+ #"System Uptime:$computer"
+ #(Get-Date) - [Management.ManagementDateTimeConverter]::ToDateTime((Get-WmiObject Win32_OperatingSystem -ComputerName $computer -Credential $cred).LastBootUpTime)  `
+ #   | Select-Object -Property Days,Hours,Minutes | FL
+
+
 
 }
